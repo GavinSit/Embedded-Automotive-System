22c07360e9c13dd191803c7573ebfdded5c2a94e
COYGooner <ivenshao@hotmail.com>
2019-12-04 21:53:50 -0500

    Update Implement.java

--- Embedded Automotive System/src/Implement.java
+++ Embedded Automotive System/src/Implement.java
@@ -2,88 +2,157 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Scanner;
 
-class Entry<K, V> {
-	final K key;
-	V value;
-	Entry<K, V> next;
-
-	public Entry(K key, V value, Entry<K, V> next) {
-		this.key = key;
-		this.value = value;
-		this.next = next;
-	}
-	// getters, equals, hashCode and toString
-}
-
-public V get(K key) {
-    Entry<K, V>[] root;
-	Entry<K, V> bucket = root[getHash(key) % getRootSize()];
-    while (bucket != null) {
-        if (bucket.key.equals(key)) {
-            return bucket.value;
-        }
-        bucket = bucket.next;
-    }
-    return null;
-}
 
 public class Implement<K,V> extends HashMap<String,String> implements HashMap<String,String>{
-	private Entry<K, V>[] root;
-	private static final int INITIAL_CAPACITY = 1 << 4; // 16
+	private Entry<K, V>[] buckets;
+	private int capacity;
 	private int size = 0;
+	
+	private double If = 0.75;
 
 	public Implement() {
-		this(INITIAL_CAPACITY);
+		this(20);
 	}
 
 	public Implement(int capacity) {
-		this.root = new Entry[capacity];
+        this.capacity = capacity;
+        this.buckets = new Entry[this.capacity];
 	}
 
 	public void put(K key, V value) {
-		Entry<K, V> entry = new Entry<>(key, value, null);
-		int bucket = getHash(key) % getRootSize();
-		Entry<K, V> existing = root[roots];
-		if (existing == null) {
-			root[bucket] = entry;
-			size++;
-		} else {
-			// compare the keys see if key already exists
-			while (existing.next != null) {
-				if (existing.key.equals(key)) {
-					existing.value = value;
-					return;
-				}
-				existing = existing.next;
-			}
-			if (existing.key.equals(key)) {
-				existing.value = value;
-			} else {
-				existing.next = entry;
-				size++;
-			}
-		}
-	}
+  if (size == lf * capacity) {
+            // rehash
+            Entry<K, V>[] old = buckets;
+
+            capacity *= 2;
+            size = 0;
+            buckets = new Entry[capacity];
+
+            for (Entry<K,V> e: old) {
+                while (e != null) {
+                    put(e.key, e.value);
+                    e = e.next;
+                }
+            }
+        }
+        Entry<K, V> entry = new Entry<>(key, value, null);
+
+        int bucket = getHash(key) % getBucketSize();
+
+        Entry<K, V> existing = buckets[bucket];
+        if (existing == null) {
+            buckets[bucket] = entry;
+            size++;
+        } else {
+            // compare the keys see if key already exists
+            while (existing.next != null) {
+                if (existing.key.equals(key)) {
+                    existing.value = value;
+                    return;
+                }
+                existing = existing.next;
+            }
+
+            if (existing.key.equals(key)) {
+                existing.value = value;
+            } else {
+                existing.next = entry;
+                size++;
+            }
+        }
+    }
+
+    public V get(K key) {
+        Entry<K, V> bucket = buckets[getHash(key) % getBucketSize()];
+
+        while (bucket != null) {
+            if (key == bucket.key) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getBucketSize() {
+        return buckets.length;
+    }
+
+    private int getHash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Entry entry : buckets) {
+            sb.append("[");
+            while (entry != null) {
+                sb.append(entry);
+                if (entry.next != null) {
+                    sb.append(", ");
+                }
+                entry = entry.next;
+            }
+            sb.append("]");
+        }
+        return "{" + sb.toString() + "}";
+    }
+
+    static class Entry<K, V> {
+        final K key;
+        V value;
+        Entry<K, V> next;
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public Entry<K, V> getNext() {
+            return next;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+
+            if (obj instanceof Entry) {
+                Entry entry = (Entry) obj;
+
+                return key.equals(entry.getKey()) &&
+                        value.equals(entry.getValue());
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 13;
+            hash = 17 * hash + ((key == null) ? 0 : key.hashCode());
+            hash = 17 * hash + ((value == null) ? 0 : value.hashCode());
+            return hash;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + ", " + value + "}";
+        }
+    }
 }
 
-@Test
-	public void testImplement() {
-		HashMap<String, String> Implement = new HashMap<>();
-	  
-		Implement.put("Sedan", "IS");
-	    Implement.put("SUV", "NX");
-	    Implement.put("Performance", "LC");
-
-	    
-	    
-	    assertNotNull(Implement);
-	  
-	    
-	    assertEquals("Sedan", Implement.get("IS"));
-	    assertEquals("SUV", Implement.get("NX"));
-	    assertEquals("Performance", Implement.get("LC"));
-	   
-	}
-}
 
 
